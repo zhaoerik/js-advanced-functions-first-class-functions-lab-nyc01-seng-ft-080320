3339fed5867bdf48ba5736907d7df7b666d83886
Erik Zhao <erikzhao98@gmail.com>
2021-01-17 20:55:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function(arr) {
-    return [arr[0], arr[1]]
+    return arr.slice(2)
 }
 
 const returnLastTwoDrivers = function(arr) {
